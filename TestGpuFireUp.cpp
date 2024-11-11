3c20df0bceee2a887334e6b787ab87e88bf7819d
Ivet Galabova <galabovaa@gmail.com>
2024-11-11 13:52:00 +0200

    updates extra-tests

---
+++ TestGpuFireUp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "catch.hpp"
+
+TEST_CASE("start-nvidia", "[test_gpu_fire_up]") {
+  // TODO add code here
+
+  std::cout << "GPU FIRE UP TEST" << std::endl;
+
+  REQUIRE(true);
+
+}
--- highs-unit-tests.cmake
+++ highs-unit-tests.cmake
@@ -1,5 +1,13 @@
 
 set(HIGHS_EXTRA_UNIT_TESTS
-    ${HIGHS_SOURCE_DIR}/check/highs-unit-tests/TestExtra.cpp)
+    ${HIGHS_SOURCE_DIR}/check/highs-unit-tests/TestGpuFireUp.cpp
+    )
 
-# message(STATUS "DEFINE TEST extra: ${HIGHS_EXTRA_UNIT_TESTS}")
+if (BUILD_CXX)
+    set(HIGHS_EXTRA_UNIT_TESTS
+        ${HIGHS_EXTRA_UNIT_TESTS} 
+        ${HIGHS_SOURCE_DIR}/check/highs-unit-tests/TestExtra.cpp
+    )
+endif()
+
+message(STATUS "DEFINE TEST extra: ${HIGHS_EXTRA_UNIT_TESTS}")
